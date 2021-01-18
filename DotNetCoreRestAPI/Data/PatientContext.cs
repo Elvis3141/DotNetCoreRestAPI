b2acae759baf6f68764f21efccfac67bb72c4488
Elvis Shehu <shehu314@hotmail.com>
2021-01-18 11:49:47 +0200

    Added Data folder and PatientContext class to execute the migration with EF core. EF core v3.1.11 and Pomelo.EntityFrameworkCore.MySql v3.2.4 packages are installed.

---
+++ DotNetCoreRestAPI/Data/PatientContext.cs
@@ -0,0 +1,25 @@
+﻿using DotNetCoreRestAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreRestAPI.Data
+{
+    public class PatientContext : DbContext
+    {
+        public PatientContext(DbContextOptions<PatientContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PersonalData> PersonalData { get; set; }
+        public DbSet<Temperature> TempeData { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseMySql("server=localhost;port=3306;database=PatientData;user=root;password=password");
+        }
+    }
+}
